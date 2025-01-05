b863538f1fc383a67d83eb3369af5b368c62de11
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-01-04 21:52:33 -0800

    created server | #3.1

---
+++ src/server.js
@@ -0,0 +1,9 @@
+import express from "express"; // "express"라는 package를 express라는 이름으로 import 한거, 경로를 안적어줘도 npm과 nodeJS가 node_modules에 가서 express 찾아서 그 안에 있는 index.js를 실행시켜줘. 와우
+
+const PORT = 4000;
+
+const app = express();
+
+const handleListening = () => console.log(`✅ Server listening on port http://localhost${PORT} 🚀`);
+
+app.listen(PORT, handleListening); // 서버는 항상 request를 기다리고 있어야해. 언제 요청하나 듣고 있는거, app.listen(port, callback)
