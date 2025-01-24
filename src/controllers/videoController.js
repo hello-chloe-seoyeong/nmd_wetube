2ff54e6ad3c876ec4c8d89162bf3b37a4a3c11ab
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-01-24 14:58:58 -0800

    Variables to Templates | #5.4

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,6 @@
-export const trending = (req, res) => res.render("home");
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
+export const trending = (req, res) => res.render("home", {pageTitle: "Home"}); // render(view이름, {템플릿에 보낼 변수 원하는 만큼 보낼 수 있어})
+export const see = (req, res) => res.render("watch", {pageTitle: "Watch"});
+export const edit = (req, res) => res.render("edit", {pageTitle: "Edit"});
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => res.send("Delete Video");
