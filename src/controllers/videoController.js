b1812dd8b8a281ed84f65cbf7e455c021bf14e35
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-01-25 20:59:54 -0800

    Iteration | #5.8

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -3,7 +3,11 @@ const fakeUser = {
   loggedIn: true,
 }
 
-export const trending = (req, res) => res.render("home", {pageTitle: "Home", fakeUser}); // render(view이름, {템플릿에 보낼 변수 원하는 만큼 보낼 수 있어})
+// export const trending = (req, res) => res.render("home", {pageTitle: "Home", fakeUser}); // render(view이름, {템플릿에 보낼 변수 원하는 만큼 보낼 수 있어})
+export const trending = (req, res) => {
+  const videos = [1,2,3,4,5,6,7,8,9,10];
+  return res.render("home", {pageTitle: "Home", videos})
+}
 export const see = (req, res) => res.render("watch", {pageTitle: "Watch"});
 export const edit = (req, res) => res.render("edit", {pageTitle: "Edit"});
 export const search = (req, res) => res.send("Search");
