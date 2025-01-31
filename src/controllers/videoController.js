b23ff66510e35905226b3993c47093c8782a1f29
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-01-30 21:37:34 -0800

    Callback function | #6.12

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,13 +2,11 @@ import Video from "../models/Video";
 // export const trending = (req, res) => res.render("home", {pageTitle: "Home", fakeUser}); // render(view이름, {템플릿에 보낼 변수 원하는 만큼 보낼 수 있어})
 
 export const home = (req, res) => {
-  Video.find({}, (error, videos) => {
+  Video.find({}, (error, videos) => { // callback function
     console.log("errors", error);
     console.log("videos", videos);
+    return res.render("home", {pageTitle: "Home", videos: []});
   });
-  // {search terms} 인데, 빈 상태로 두면 "모든 형식"을 찾는다는 것을 의미
-  return res.render("home", {pageTitle: "Home", videos: []});
-  // home.pug에서 videos 배열 기다리고 있어서 임시로 보내주는 거
 }
 export const watch = (req, res) => {
   // const id = req.params.id; == same, 아래꺼가 ES6
