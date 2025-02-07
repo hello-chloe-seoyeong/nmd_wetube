a90ea5482400781fdb1b659eb18118fbb2412347
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-02-06 17:03:00 -0800

    Delete Video | #6.25

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { watch, getEdit, postEdit, getUpload, postUpload } from "../controllers/videoController";
+import { watch, getEdit, postEdit, getUpload, postUpload, deleteVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
@@ -7,6 +7,7 @@ videoRouter.get("/:id([0-9a-f]{24})", watch);
 // videoRouter.get("/:id(\\d+)/edit", getEdit);
 // videoRouter.post("/:id(\\d+)/edit", postEdit);
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
 // videoRouter.get("/upload", getUpload);
 // videoRouter.post("/upload", postUpload);
 videoRouter.route("/upload").get(getUpload).post(postUpload); // videoRouter : /videos, /upload: /upload
