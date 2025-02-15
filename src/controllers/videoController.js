ded53b4b1aadfde523db46ddd1b9154cdd97dfe8
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-02-14 16:35:31 -0800

    Status Codes | #7.4

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -15,7 +15,7 @@ export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
   if(!video) {
-    res.render("404", {pageTitle: "Video not found"})
+    res.status(404).render("404", {pageTitle: "Video not found"})
   }
   res.render("watch", {pageTitle: video.title, video})
 };
@@ -24,7 +24,7 @@ export const getEdit = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
   if(!video) {
-    res.render("404", {pageTitle: "Video not found"})
+    res.status(404).render("404", {pageTitle: "Video not found"})
   }
   res.render("edit", {pageTitle: `Edit: ${video.title}`, video})
 };
