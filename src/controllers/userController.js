34956d8b54a5ce1f0a0294603bd9875cd85ad938
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-02-20 15:41:06 -0800

    Github Login part two | #7.17

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -65,6 +65,24 @@ export const postLogin = async (req, res) => {
   console.log(req.session)
   return res.redirect("/");
 }
+
+export const startGithubLogin = (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/authorize";
+  const config = {
+    client_id: "Ov23liH9f5j1KcyannDY",
+    allow_signup: false,
+    scope: "read:user user:email"
+  }
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+
+  return res.redirect(finalUrl);
+}
+
+export const finishGithubLogin = (req, res) => {
+
+}
+
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Delete User");
 export const logout = (req, res) => res.send("Logout");
