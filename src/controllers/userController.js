99469b065a8f808291e7a1ad2fc9555ebbd7a6fb
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-02-24 13:57:22 -0800

    Github Login part five | #7.20

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -100,14 +100,28 @@ export const finishGithubLogin = async (req, res) => {
   if("access_token" in tokenRequest) {
     // access api
     const { access_token } = tokenRequest;
-    const userRequest = await (
-      await fetch("https://api.github.com/user", {
+    const apiUrl = "https://api.github.com";
+
+    const userData = await (
+      await fetch(`${apiUrl}/user`, {
         headers: {
           Authorization: `token ${access_token}`
         }
       })
     ).json();
-    console.log(userRequest)
+    const emailData = await (
+      await fetch(`${apiUrl}/user/emails`, {
+        headers: {
+          Authorization: `token ${access_token}`
+        }
+      })
+    ).json();
+    console.log(userData)
+    console.log(emailData)
+    const email = emailData.find((email) => email.primary === true && email.verified === true);
+    if(!email) {
+      return res.redirect("/login")
+    }
   } else {
     return res.redirect("/login")
   }
