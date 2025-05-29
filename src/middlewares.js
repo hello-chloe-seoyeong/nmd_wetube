3ce69892f4665828bf834eff25184a8417201984
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-05-28 21:20:53 -0700

    Edit Profile | #8.0

--- src/middlewares.js
+++ src/middlewares.js
@@ -5,7 +5,6 @@ export const localsMiddleware = (req, res, next) => {
   // console.log(req.session);
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "Wetube";
-  res.locals.loggedInUser = req.session.user;
+  res.locals.loggedInUser = req.session.user || {};
   next();
-}
-
+};
