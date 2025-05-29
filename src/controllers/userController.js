6df13654b073a97f461c6db9a90e88b8a7149e70
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-05-28 21:04:17 -0700

    prettier?

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,22 +1,22 @@
 import User from "../models/User";
 import bcrypt from "bcrypt";
 
-export const getJoin = (req, res) => res.render("join", {pageTitle: "Join"});
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
   const { name, email, username, password, password2, location } = req.body;
   const pageTitle = "Join";
-  if(password !== password2) {
-    return res.status(400).render('join', {
+  if (password !== password2) {
+    return res.status(400).render("join", {
       pageTitle,
-      errorMessage: "Password confirmation does not matched."
-    })
+      errorMessage: "Password confirmation does not matched.",
+    });
   }
-  const exists = await User.exists({ $or: [{username}, {email}]})
-  if(exists) {
-    return res.status(400).render('join', {
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  if (exists) {
+    return res.status(400).render("join", {
       pageTitle,
-      errorMessage: "This username/email is already taken.."
-    })
+      errorMessage: "This username/email is already taken..",
+    });
   }
 
   try {
@@ -25,14 +25,18 @@ export const postJoin = async (req, res) => {
       email,
       password,
       username,
-      location
-    })
+      location,
+    });
     return res.redirect("/login");
   } catch (err) {
-    return res.status(400).render("join", { pageTitle: "Upload Video", errorMessage: err._message})
+    return res.status(400).render("join", {
+      pageTitle: "Upload Video",
+      errorMessage: err._message,
+    });
   }
-}
-export const getLogin = (req, res) => res.render("login", { pageTitle: "Login"});
+};
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
 
 export const postLogin = async (req, res) => {
   // check if account exists
@@ -42,63 +46,63 @@ export const postLogin = async (req, res) => {
 
   // 계정이 존재하는지, 비밀번호가 맞는지 확인하려면 "누구"의 비밀번호인지 유저를 2번 찾네? 한번에 합쳐주기
   const pageTitle = "Login"; // 2번 겹치니까 변수로 써서 활용
-  console.log(user)
-  if(!user) {
+  console.log(user);
+  if (!user) {
     return res.status(400).render("login", {
       pageTitle,
-      errorMessage: "An account with this username does not exists."
-    })
+      errorMessage: "An account with this username does not exists.",
+    });
   }
   // check if password incorrect
   // const user = await User.findOne({ username });
   // console.log(user.password);
 
   const ok = await bcrypt.compare(password, user.password);
-  if(!ok) {
+  if (!ok) {
     return res.status(400).render("login", {
       pageTitle,
-      errorMessage: "Wrong password"
-    })
+      errorMessage: "Wrong password",
+    });
   }
   // req.session: session object에 로그인 정보 추가
   req.session.loggedIn = true;
   req.session.user = user;
-  console.log(req.session)
+  console.log(req.session);
   return res.redirect("/");
-}
+};
 
 export const startGithubLogin = (req, res) => {
   const baseUrl = "https://github.com/login/oauth/authorize";
   const config = {
     client_id: process.env.GH_CLIENT,
     allow_signup: false,
-    scope: "read:user user:email"
-  }
+    scope: "read:user user:email",
+  };
   const params = new URLSearchParams(config).toString();
   const finalUrl = `${baseUrl}?${params}`;
 
   return res.redirect(finalUrl);
-}
+};
 
 export const finishGithubLogin = async (req, res) => {
   const baseUrl = "https://github.com/login/oauth/access_token";
   const config = {
     client_id: process.env.GH_CLIENT,
     client_secret: process.env.GH_SECRET,
-    code: req.query.code
-  }
+    code: req.query.code,
+  };
   const params = new URLSearchParams(config).toString();
   const finalUrl = `${baseUrl}?${params}`;
   const tokenRequest = await (
     await fetch(finalUrl, {
       method: "POST",
       headers: {
-        Accept: "application/json"
-      }
+        Accept: "application/json",
+      },
     })
   ).json();
   // res.send(JSON.stringify(json));
-  if("access_token" in tokenRequest) {
+  if ("access_token" in tokenRequest) {
     // access api
     const { access_token } = tokenRequest;
     const apiUrl = "https://api.github.com";
@@ -106,25 +110,29 @@ export const finishGithubLogin = async (req, res) => {
     const userData = await (
       await fetch(`${apiUrl}/user`, {
         headers: {
-          Authorization: `token ${access_token}`
-        }
+          Authorization: `token ${access_token}`,
+        },
       })
     ).json();
     const emailData = await (
       await fetch(`${apiUrl}/user/emails`, {
         headers: {
-          Authorization: `token ${access_token}`
-        }
+          Authorization: `token ${access_token}`,
+        },
       })
     ).json();
-    console.log(userData)
-    console.log(emailData)
-    const emailObj = emailData.find((email) => email.primary === true && email.verified === true);
-    if(!emailObj) {
-      return res.redirect("/login")
+    console.log(userData);
+    console.log(emailData);
+    const emailObj = emailData.find(
+      (email) => email.primary === true && email.verified === true
+    );
+    if (!emailObj) {
+      // set notification
+      return res.redirect("/login");
     }
     let user = await User.findOne({ email: emailObj.email });
-    if(!user) { // 동일한 이메일이 있으면 그냥 바로 로그인 시켜주기
+    if (!user) {
+      // 동일한 이메일이 있으면 그냥 바로 로그인 시켜주기
       // create an account
       user = await User.create({
         avatarUrl: userData.avatar_url,
@@ -134,16 +142,16 @@ export const finishGithubLogin = async (req, res) => {
         username: userData.login,
         location: userData.location,
         socialOnly: true,
-      })
+      });
     }
 
     req.session.loggedIn = true;
     req.session.user = user;
     return res.redirect("/");
   } else {
-    return res.redirect("/login")
+    return res.redirect("/login");
   }
-}
+};
 export const logout = (req, res) => {
   req.session.destroy();
   return res.redirect("/");
