50bab5fdace8f6067d543bfa1b8e331c1d8e5dab
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-06-04 08:26:00 -0700

    File upload part two | #8.7

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -163,7 +163,7 @@ export const getEdit = (req, res) => {
 export const postEdit = async (req, res) => {
   const {
     session: {
-      user: { _id },
+      user: { _id, avatarUrl },
     },
     body: { name, email, username, location },
   } = req;
@@ -181,6 +181,7 @@ export const postEdit = async (req, res) => {
   const updatedUser = await User.findByIdAndUpdate(
     _id,
     {
+      avatarUrl: file ? file.path : avatarUrl,
       name,
       email,
       username,
