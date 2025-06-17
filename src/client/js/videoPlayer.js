afd449aac89092d2f590c0bc97d117e19b32111f
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-06-17 15:00:06 -0700

    Play Pause | #11.1

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,24 @@
-console.log("video player");
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+const handlePlayClick = (e) => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+};
+
+// handlePlayClick 안에 각각 사용해서 글자를 바꿀수 있지만, 함수를 만들어서 video에 eventListener 로 해주자.
+const handlePause = () => (playBtn.innerText = "Play");
+const handlePlay = () => (playBtn.innerText = "Pause");
+
+const handleMute = (e) => {};
+
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMute);
+video.addEventListener("pause", handlePause);
+video.addEventListener("play", handlePlay);
