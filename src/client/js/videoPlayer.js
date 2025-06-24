a4dac1d51c286ae134e18f0b1b1e28bbac15929d
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-06-24 14:48:39 -0700

    Time Formatting | #11.5

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -58,12 +58,15 @@ const handleVolumeChange = (event) => {
   volumeValue = value;
 };
 
+const formatTime = (seconds) =>
+  new Date(seconds * 1000).toISOString().substring(11, 8);
+
 const handleLoadMetadata = () => {
-  totalTime.innerText = Math.floor(video.duration);
+  totalTime.innerText = formatTime(Math.floor(video.duration));
 };
 
 const handleTimeUpdate = () => {
-  currentTime.innerText = Math.floor(video.currentTime);
+  currentTime.innerText = formatTime(Math.floor(video.currentTime));
 };
 
 playBtn.addEventListener("click", handlePlayClick);
