271d397f5b09e29ac30c2119bafd2cf0006e8d49
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-06-24 20:24:54 -0700

    Register View Controller | #12.0

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import { localsMiddleware } from "./middlewares";
+import apiRouter from "./routers/apiRouter";
 
 const app = express();
 const logger = morgan("dev");
@@ -37,6 +38,7 @@ app.use("/static", express.static("assets"));
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
+app.use("/api", apiRouter);
 
 // server의 configuration에 관련 코드 처리 후 export
 
