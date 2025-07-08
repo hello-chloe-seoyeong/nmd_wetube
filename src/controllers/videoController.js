35186e129fb21ccdbc6e90b51f6de81070f85ea9
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-07-07 22:18:25 -0700

    Rendering Comments | #16.6

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -19,7 +19,7 @@ export const watch = async (req, res) => {
   const { id } = req.params;
   // const video = await Video.findById(id);
   // const owner = await User.findById(video.owner);
-  const video = await Video.findById(id).populate("owner");
+  const video = await Video.findById(id).populate("owner").populate("comments");
 
   if (!video) {
     res.status(404).render("404", { pageTitle: "Video not found" });
@@ -170,5 +170,7 @@ export const createComment = async (req, res) => {
     owner: user._id,
     video: id,
   });
+  video.comments.push(comment._id);
+  video.save();
   return res.sendStatus(201);
 };
